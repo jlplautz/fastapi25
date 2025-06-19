c1c682ccbc9cbaebb5e1b4892c6cbeec7abc9a54
Jorge Luis Plautz <jorge.plautz@gmail.com>
2025-06-19 19:06:21 -0300

    3-aula2 criar_schemas

--- fastapi25/app.py
+++ fastapi25/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from fastapi25.schemas import Message
+
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.get('/ola_mundo', status_code=200)
+def ola_mundo():
+    return {"Ola Mundo!"}
